79e709c6bbd3ea316a17421759a4ee704e3d79d5
vo1200 <vanessaoru@yahoo.com>
2023-02-10 23:05:48 -0500

    nav font issue fixed

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="../../public/Brand.woff" rel="stylesheet" type="text/css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
