476e7dbb159ce52d9b7142cb7f5ef051dee8d292
vo1200 <vanessaoru@yahoo.com>
2023-02-10 20:37:19 -0500

    nav font issue fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5rem;
+  font-family: "Brand";
+`;
 
 function Nav() {
   return (
-    <nav>
+    <NavContainer>
       <Link href="/">Vanessa&apos;s Blog Site</Link>
-    </nav>
+    </NavContainer>
   );
 }
 
